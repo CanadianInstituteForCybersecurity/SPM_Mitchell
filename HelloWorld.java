ff861f93717d8d83032919bf0eed50514449ba61
mfmacdonald2 <91632233+mfmacdonald2@users.noreply.github.com>
2024-09-13 15:24:16 -0300

    Just an edit to the ReadME

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+// Simple hello world program uploaded to test github and travis CI
+
+public class HelloWorld{
+
+    public static void main(String[] args){
+        System.out.println("Hello Word");
+    }
+}
