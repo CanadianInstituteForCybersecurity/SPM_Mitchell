a27fd7e4dbe14e062e9bd5541834d23f3c07474d
mfmacdonald2 <91632233+mfmacdonald2@users.noreply.github.com>
2024-09-13 15:58:46 -0300

    Switched to python

---
+++ bottleSong.py
@@ -0,0 +1,14 @@
+#Silly song to test using github and travis ci
+
+beer = 99
+
+while (beer > 1):
+    if (beer > 2):
+        print(beer, " bottles of beer on the wall, ",beer," bottles of beer, take one down pass it around ", beer-1, " bottles of beer on the wall")
+        beer = beer-1
+    else:
+        print(beer, " bottles of beer on the wall, ",beer," bottles of beer, take one down pass it around ", beer-1, " bottle of beer on the wall")
+        beer = beer-1
+
+print("One bottle of beer on the wall, one bottle of beer, take it down pass it around no more bottles of beer on the wall")
+beer = beer-1
